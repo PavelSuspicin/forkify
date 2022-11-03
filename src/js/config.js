1ccb3482b8c4fe98a9c691862c50260777014475
Pavel <pavelsuspicin@gmail.com>
2022-11-03 09:21:45 +0300

    03.11.22

--- src/js/config.js
+++ src/js/config.js
@@ -1,2 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/'
 export const TIMEOUT_SEC = 10
+export const RES_PER_PAGE = 10
+export const KEY = 'fffe9516-f2fd-4473-919e-e5407e59bfb2'
+export const MODAL_CLOSE_SEC = 2.5
--- src/js/controller.js
+++ src/js/controller.js
@@ -1,12 +1,22 @@
 import * as model from './model'
-import recipeView from '../js/views/recipeView'
-import searchView from '../js/views/searchView'
+import { MODAL_CLOSE_SEC } from './config'
+import recipeView from './views/recipeView'
+import searchView from './views/searchView'
+import resultsView from './views/resultsView'
+import paginationView from './views/paginationView'
+import bookmarksView from './views/bookmarksView'
+import addRecipeView from './views/addRecipeView'
 
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
-import { async } from 'regenerator-runtime'
+// import { async } from 'regenerator-runtime'
+
+// if (module.hot) {
+//   module.hot.accept()
+// }
 
 // https://forkify-api.herokuapp.com/v2
+// fffe9516-f2fd-4473-919e-e5407e59bfb2
 
 const controlRecipes = async function () {
   try {
@@ -15,18 +25,27 @@ const controlRecipes = async function () {
     if (!id) return
     recipeView.renderSpinner()
 
-    // 1.Load recipe
+    // Update results view to mark selected search result
+    resultsView.update(model.getSearchResultsPage())
+
+    // Updating bookmark view
+    bookmarksView.update(model.state.bookmarks)
+
+    // Load recipe
     await model.loadRecipe(id)
 
-    //2.Render recipe
+    // Render recipe
     recipeView.render(model.state.recipe)
   } catch (error) {
     recipeView.renderError()
+    console.error(error)
   }
 }
 
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner()
+
     // 1.Get search query
     const query = searchView.getQuery()
     if (!query) return
@@ -35,14 +54,83 @@ const controlSearchResults = async function () {
     await model.loadSearchResults(query)
 
     // 3.Render results
-    console.log(model.state.search.results)
+    // resultsView.render(model.state.search.results)
+    resultsView.render(model.getSearchResultsPage())
+
+    // 4 Render initial pagination buttons
+    paginationView.render(model.state.search)
   } catch (error) {
     console.error(error)
   }
 }
 
+const controlPagination = function (goToPage) {
+  // 1.Render new results
+  resultsView.render(model.getSearchResultsPage(goToPage))
+
+  // 2. Render new pagination buttons
+  paginationView.render(model.state.search)
+}
+
+const controlServings = function (newServings) {
+  // Update the recipe servings
+  model.updateServings(newServings)
+
+  // Update the recipe view
+  // recipeView.render(model.state.recipe)
+  recipeView.update(model.state.recipe)
+}
+
+const controlAddBookmark = function () {
+  // Add/remove bookmark
+  if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe)
+  else model.deleteBookmark(model.state.recipe.id)
+
+  // Update recipe view
+  recipeView.update(model.state.recipe)
+
+  // Render bookmarks
+  bookmarksView.render(model.state.bookmarks)
+}
+
+const controlBookmarks = function () {
+  bookmarksView.render(model.state.bookmarks)
+}
+
+const controlAddRecipe = async function (newRecipe) {
+  try {
+    // Show loading spinner
+    addRecipeView.renderSpinner()
+
+    // Upload new recipe data
+    await model.uploadRecipe(newRecipe)
+    console.log(model.state.recipe)
+
+    // Render recipe
+    recipeView.render(model.state.recipe)
+
+    // Success message
+    addRecipeView.renderMessage()
+
+    // Close form window
+    setTimeout(function () {
+      addRecipeView.toggleWindow()
+    }, MODAL_CLOSE_SEC * 1000)
+  } catch (error) {
+    console.error(error)
+    addRecipeView.renderError(err.message)
+  }
+}
+
 const init = function () {
+  bookmarksView.addHandlerRender(controlBookmarks)
+
   recipeView.addHandlerRender(controlRecipes)
+  recipeView.addHandlerUpdateServings(controlServings)
+  recipeView.addHandlerAddBookmark(controlAddBookmark)
+
   searchView.addHandlerSearch(controlSearchResults)
+  paginationView.addHandlerClick(controlPagination)
+  addRecipeView.addHandlerUpload(controlAddRecipe)
 }
 init()
