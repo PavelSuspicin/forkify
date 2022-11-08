bca28635e0c47e7ea61f251b47d583000e82e40d
Pavel <pavelsuspicin@gmail.com>
2022-11-08 10:00:00 +0300

    new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -128,6 +128,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 }
 
+const newFeature = function () {
+  console.log('welcome to the application')
+}
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks)
 
@@ -138,6 +142,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults)
   paginationView.addHandlerClick(controlPagination)
   addRecipeView.addHandlerUpload(controlAddRecipe)
-  console.log('hello world')
+  newFeature()
 }
 init()
